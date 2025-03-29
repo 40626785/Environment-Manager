dfceb0eb50d444c68eb1037f99f2ebf75a6a60d6
Nataniel Stachowiak <40646470@live.napier.ac.uk>
2025-03-29 21:16:53 +0000

    note mvvm practical working as requested (still not conviced this will be the optmimal way to start)

---
+++ notes/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace notes;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
